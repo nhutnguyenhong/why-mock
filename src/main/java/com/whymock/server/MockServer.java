9a01761c9f4d96e9b7873c1dbcd9ef8cd49cdac3
nhutnguyen <nhut.nguyen@tyme.com>
2021-04-05 10:23:20 +0700

    Deploy at the config port

--- src/main/java/com/whymock/server/MockServer.java
+++ src/main/java/com/whymock/server/MockServer.java
@@ -1,15 +1,13 @@
 package com.whymock.server;
 
+import static com.github.tomakehurst.wiremock.core.WireMockApp.MAPPINGS_ROOT;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.whymock.mapping.CustomMappingSource;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-
-import java.io.IOException;
-
-import static com.github.tomakehurst.wiremock.core.WireMockApp.MAPPINGS_ROOT;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 @Slf4j
 public class MockServer {
@@ -25,8 +23,8 @@ public class MockServer {
         wireMockServer.start();
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String port = StringUtils.defaultIfEmpty(System.getProperty("server.port"),"5868") ;
+    public static void main(String[] args) {
+        String port = StringUtils.defaultIfEmpty(System.getProperty("server.port"), "5868");
         System.out.println("Start mocking server at port " + port);
         startServer(port);
     }
